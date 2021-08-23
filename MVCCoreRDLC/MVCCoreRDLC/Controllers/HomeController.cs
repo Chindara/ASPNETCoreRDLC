8af4c4e8f28e0b6c639476cb7a6950dfcdd884b8
Chinthaka Bandara <chinthakab@dmsswe.com>
2021-08-23 17:27:53 +0530

    [feat] excel, word exports

--- MVCCoreRDLC/MVCCoreRDLC/Controllers/HomeController.cs
+++ MVCCoreRDLC/MVCCoreRDLC/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace MVCCoreRDLC.Controllers
             return View();
         }
 
-        public IActionResult Print()
+        public IActionResult PDF()
         {
             DataTable dt = GetData();
 
@@ -38,7 +38,45 @@ namespace MVCCoreRDLC.Controllers
 
             var result = localReport.Execute(RenderType.Pdf, extension, parameters, mimetype);
 
-            return File(result.MainStream, "application/pdf");
+            return File(result.MainStream, "application/pdf", "Employees.pdf");
+        }
+
+        public IActionResult Excel()
+        {
+            DataTable dt = GetData();
+
+            string mimetype = "";
+            int extension = 1;
+            var path = $"{this._webHostEnvironment.WebRootPath}\\reports\\Report1.rdlc";
+
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("prm", "DMS Software Engineering");
+
+            LocalReport localReport = new LocalReport(path);
+            localReport.AddDataSource("dsEmployee", dt);
+
+            var result = localReport.Execute(RenderType.Excel, extension, parameters, mimetype);
+
+            return File(result.MainStream, "application/msexcel", "Employees.xls");
+        }
+
+        public IActionResult Word()
+        {
+            DataTable dt = GetData();
+
+            string mimetype = "";
+            int extension = 1;
+            var path = $"{this._webHostEnvironment.WebRootPath}\\reports\\Report1.rdlc";
+
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("prm", "DMS Software Engineering");
+
+            LocalReport localReport = new LocalReport(path);
+            localReport.AddDataSource("dsEmployee", dt);
+
+            var result = localReport.Execute(RenderType.Word, extension, parameters, mimetype);
+
+            return File(result.MainStream, "application/msword", "Employees.doc");
         }
 
         public DataTable GetData()
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ASP.NET Core MVC with RDLC Reports
 ASP.NET Core MVC with RDLC Reports
+
+##Supported Outputs
+- PDF
+- Excel
+- Word
+
+##Prerequisites
+- .NET Core 3.1
+- .NET Framework 4.7.2
+
+##Nuget Packages Used
+- AspNetCore.Reporting (v2.1.0)
+- System.CodeDom (v.5.0.0)
+- System.Data.SqlClient (v4.8.2)
